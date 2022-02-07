0267f74c8d79729f0cb4d43a41aed44996e2120c
athul23p <athul238p@gmail.com>
2022-02-07 15:45:51 +0530

    added checkout functionality

---
+++ scripts/checkout.js
@@ -0,0 +1,17 @@
+
+document.querySelector('form').addEventListener('submit',placeOrder);
+
+function placeOrder(event){
+    event.preventDefault();
+
+    setTimeout(function(){
+        alert('Your order is accepted')},0);
+    setTimeout(function(){
+        alert('Your order is being cooked')},3000);
+    setTimeout(function(){
+        alert('Your order is ready')},8000);
+    setTimeout(function(){
+        alert('Order out for delivery')},10000);
+    setTimeout(function(){
+        alert('Order delivered')},12000);
+}
