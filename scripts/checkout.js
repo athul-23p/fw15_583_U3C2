9d98ced61d6c3d153c5820b2cb51c6ad1cfa9a29
athul23p <athul238p@gmail.com>
2022-02-07 16:48:19 +0530

    added validation to checkout form

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,16 +4,26 @@ document.querySelector('form').addEventListener('submit',placeOrder);
 function placeOrder(event){
     event.preventDefault();
 
-    setTimeout(function(){
-        alert('Your order is accepted')},0);
-    setTimeout(function(){
-        alert('Your order is being cooked')},3000);
-    setTimeout(function(){
-        alert('Your order is ready')},8000);
-    setTimeout(function(){
-        alert('Order out for delivery')},10000);
-    setTimeout(function(){
-        alert('Order delivered')},12000);
+    let name = document.getElementById('name').value;
+    let number = document.getElementById('number').value;
+    let address = document.getElementById('address').value;
 
-    localStorage.removeItem('cart');
+    if(name!='' && number!='' && address!= ''){
+
+        setTimeout(function(){
+            alert('Your order is accepted')},0);
+        setTimeout(function(){
+            alert('Your order is being cooked')},3000);
+        setTimeout(function(){
+            alert('Your order is ready')},8000);
+        setTimeout(function(){
+            alert('Order out for delivery')},10000);
+        setTimeout(function(){
+            alert('Order delivered')},12000);
+    
+        localStorage.removeItem('cart');
+    }
+    else{
+        alert('Please fill all the fields');
+    }
 }
